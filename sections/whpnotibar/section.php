ae611713859b222e21eccac7b866ea57d74fc969
Raymond Lopez Aleman <hello@raylo.net>
2014-08-06 17:17:51 -0700

    Header & Notibar

--- sections/whpnotibar/section.php
+++ sections/whpnotibar/section.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Section: WHiteHousePro Notibar
+	Section: WhiteHousePro Notibar
 	Author: PageLines
 	Author URI: http://www.pagelines.com
 	Description: A simple optimization tool that allows you to show the right message at the right time to your website visitors
@@ -12,81 +12,76 @@ class WHPNotibar extends PageLinesSection {
 
 	function section_styles(){
 
-		wp_enqueue_script( 'revolution-plugins', $this->base_url.'/whoahbar.js', array( 'jquery' ), pl_get_cache_key(), true );
+		wp_enqueue_script( 'jbar', $this->base_url.'/jbar.min.js', array( 'jquery' ), true );
 
 	}
 	
 
 	function section_opts(){
-		$opts = array(
-			array(
-				'col'		=> 2,
-				'type' 		=> 'textarea',
-				'key'		=> 'ishoutbox_content',
-				'label'		=> __( 'iShoutBox Content', 'pagelines' ),
-				'help' 		=> __( 'This area supports text and HTML', 'pagelines' ),
-			),
-			array(
-				'type'		=> 'multi',
-				'key'		=> 'ishoutbox_settings', 
-				'label' 	=> __( 'iShoutBox Settings', 'pagelines' ),
-				'opts'		=> array(
-					array(
-						'type' 			=> 'select',
-						'key'			=> 'ishoutbox_align',
-						'label' 		=> 'Alignment',
-						'opts'			=> array(
-							'textcenter'	=> array('name' => 'Center (Default)'),
-							'textleft'		=> array('name' => 'Align Left'),
-							'textright'		=> array('name' => 'Align Right'),
-							'textjustify'	=> array('name' => 'Justify'),
-						)
-					),	
+
+		$options = array();
+
+		$options[] = array(
+
+			'title' => __( 'Notibar Configuration', 'pagelines' ),
+			'type'	=> 'multi',
+			'col'		=> 1,
+			'opts'	=> array(
 				array(
-						'key'		=> 'ishoutbox_pad',
-						'type' 		=> 'text',
-						'label' 	=> __( 'Padding <small>(CSS Shorthand)</small>', 'pagelines' ),
-						'ref'		=> __( 'This option uses CSS padding shorthand. For example, use "15px 30px" for 15px padding top/bottom, and 30 left/right.', 'pagelines' ),
-						'default' 	=> '5px',
-						
-					),				
-				)
-			),
+					'type' 		=> 'text',
+					'key'		=> 'notibar_text',
+					'label'		=> __( 'Main Text <span class="badge badge-mini badge-warning">REQUIRED</span>', 'pagelines' ),
+					'default'	=> 'Check Out PageLines DMS for WordPress',
+					'help' 		=> __( 'This area supports text and HTML', 'pagelines' ),
+				),
+				array(
+					'type' 		=> 'text',
+					'key'		=> 'notibar_button',
+					'default'	=> 'PageLines',
+					'label'		=> __( 'Button Text', 'pagelines' ),
+				),
+				array(
+					'type' 		=> 'text',
+					'key'		=> 'notibar_button_url',
+					'default'	=> 'http://www.pagelines.com/',
+					'label'		=> __( 'Button Link', 'pagelines' ),
+				),
+				array(
+					'type' 		=> 'select',
+					'key'		=> 'notibar_state',
+					'default'	=> 'open',
+					'label'		=> __( 'Button State', 'pagelines' ),
+					'opts'		=> array(
+						'open'		=> array('name' => 'Open (Default)'),
+						'closed'	=> array('name' => 'Closed'),
+					)
+				),	
+			)
+
 		);
 
-		return $opts;
+		return $options;
 
 	}
 
 
 	function section_template() {
 
-		$content = $this->opt('ishoutbox_content');
+		$text = ($this->opt('notibar_text', $this->oset)) ? $this->opt('notibar_text', $this->oset) : 'Notibar is Ready';
 		
-		$content = (!$content) ? '<p><strong>iShoutBox</strong> &raquo; Add Content or any HTML!</p>' : sprintf('%s', do_shortcode( wpautop($content) ) ); 
-			
-		$align = ($this->opt('ishoutbox_align', $this->oset)) ? $this->opt('ishoutbox_align', $this->oset) : 'center';
-		
-		$padding = ($this->opt('ishoutbox_pad')) ? sprintf('padding: %s;', $this->opt('ishoutbox_pad')) : ''; 
-		
-		
-		//printf('<div class="ishoutbox-wrap fade in %s" style="%s">%s <button type="button" class="close-ishoutbox" href="#" data-dismiss="alert">×</button></div>', $align, $padding, $content);
-		
-		?>
+		$button = ($this->opt('notibar_button', $this->oset)) ? $this->opt('notibar_button', $this->oset) : 'Engage your visitors!';
 
-		
-		<div class="woahbar" style="display:none">
-		   <span>
-		       Welcome to Jobdeals! Need some help from a local service pro? <a class="woahbar-link" href="/request-services/">Request a Service FREE</a>
-		    </span>
-		    <a class="close-notify" onclick="woahbar_hide();"><img class="woahbar-up-arrow" src="woahbar-up-arrow.png"></a>
-		</div>
-		<div class="woahbar-stub" style="display:none">
-		    <a class="show-notify" onclick="woahbar_show();"><img class="woahbar-down-arrow" src="woahbar-down-arrow.png"></a>
-		</div>
-		
+		$button_url = ($this->opt('notibar_button_url', $this->oset)) ? $this->opt('notibar_button_url', $this->oset) : 'http://pagelines.com';
 
-		<?php 
+		$state = ($this->opt('notibar_state', $this->oset)) ? $this->opt('notibar_state', $this->oset) : 'open'; ?>
+		
+				
+		<div class="jbar whpnotibar" data-init="jbar" data-jbar='{
+			"message" : "<?php echo $text; ?> <a href=\"<?php echo $button_url; ?>\" class=\"whpnotibar-button\"><?php echo $button; ?></a>",
+			"state"   : "<?php echo $state; ?>"
+		}'></div> 
+	
+	<?php 
 	}
 }
 
